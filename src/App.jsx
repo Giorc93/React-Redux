263db49a9819c3182fcfa1fe007eaf81d3e6e746
Gio <giorc93@hotmail.com>
2020-12-08 20:59:01 -0500

    Second Commit: App name changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import React from "react";
+
+//Importing components
+import PokemonsComponent from "./components/PokemonsComponent";
+
+//Import generateStore
+import generateStore from "./redux/store";
+
+//Import provider
+import { Provider } from "react-redux";
 
 function App() {
-  return <div></div>;
+  const store = generateStore();
+
+  return (
+    <Provider store={store}>
+      <PokemonsComponent />
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/components/PokemonsComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const PokemonsComponent = () => {
+  return (
+    <Fragment>
+      <h2>Pokemons List</h2>
+    </Fragment>
+  );
+};
+
+export default PokemonsComponent;
